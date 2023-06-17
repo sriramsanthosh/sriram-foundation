1ea8216f0828cfb4e7928f7fe5faa613500fdc82
Itha Sriram Santhosh <isrs444@gmail.com>
2023-06-17 23:58:59 +0530

    update-contact-form

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,31 @@
-var flag = true;
+var flag = false;
 var temp3 = document.querySelector('.nav-list');
 var temp4 = document.querySelectorAll('.nav-link');
+var temp5 = document.querySelector('main');
+var temp6 = document.querySelector('.donate-bg');
+var temp7 = document.querySelector('footer');
+temp3.style.top = '-100vh';
+
+function toggleNavbar(){
+    var ex = toString(temp3.style.top);
+    if(ex != "-100vh"){
+        temp3.style.top = '-100vh';
+        flag = false;
+    }
+}
+
+temp5.addEventListener('click', toggleNavbar);
+temp6.addEventListener('click', toggleNavbar);
+temp7.addEventListener('click', toggleNavbar);
+
 for(let k = 0; k<temp4.length; k++){
     temp4[k].addEventListener('click', function(){
         if(flag){
-            temp3.style.left = '-50%';
+            temp3.style.top = '-100vh';
         }
         else{
-            temp3.style.left = '0';
+            temp3.style.top = '0';
         }
         flag = !flag;
     });
 }
-
-var paynowButton = document.querySelector('.paynow');
-var colors = ["red", "blue", "hotpink", "green", "violet", "skyblue"];
-
-// paynowButton.style.transition = '0.4s';
-
-let l = 0;
-var lightingButton = setInterval(function(){
-        let hel = "10px 20px 30px "+colors[l];
-        paynowButton.style.boxShadow = hel;
-        if(l == colors.length){
-            l = -1;
-        }
-        l++;
-}, 1000);
