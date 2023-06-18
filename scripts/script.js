158566eacb60c48ab122517faecacc4fc84681d9
Itha Sriram Santhosh <isrs444@gmail.com>
2023-06-18 18:12:51 +0530

    close to 1.0

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,49 @@ for(let k = 0; k<temp4.length; k++){
         flag = !flag;
     });
 }
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAj71I1MUrbJpbuiEV9OHqLhrN9i4uYPqc",
+    authDomain: "sriram-foundation.firebaseapp.com",
+    databaseURL: "https://sriram-foundation-default-rtdb.firebaseio.com",
+    projectId: "sriram-foundation",
+    storageBucket: "sriram-foundation.appspot.com",
+    messagingSenderId: "425119094940",
+    appId: "1:425119094940:web:4047d361d00ac746373559",
+    measurementId: "G-FYLP5J3C02"
+};
+
+//   initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+//   reference your database 
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.querySelector('.input-form').addEventListener('submit', submitForm);
+
+
+function submitForm(e){
+    e.preventDefault();
+    var firstName = document.querySelector('#Fname').value;
+    var lastName = document.querySelector('#Lname').value;
+    var phone = document.querySelector('#phone').value;
+    var email = document.querySelector('#email').value;
+    var msgcontent = document.querySelector('#message').value;
+
+    let date = new Date().toJSON();
+    saveContent(firstName, lastName, email, phone, msgcontent, date);
+    window.alert("Thank you!! We will get back to you soon!");
+}
+
+const saveContent = (firstName, lastName, email, phone, msgcontent, currentDate)=>{
+    var newContactForm = contactFormDB.push();
+    newContactForm.set({
+        FName : firstName,
+        LName : lastName,
+        email : email,
+        phone : phone,
+        msgcontent : msgcontent,
+        date : currentDate
+    });
+}
